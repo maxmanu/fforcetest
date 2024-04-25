76d010b87286575d08b3788826bd1ff81472c399
Max Manuel <maxcamina@gmail.com>
2024-04-25 13:01:40 -0500

    first commit

---
+++ wp-home-test.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: WP Home Test
+Plugin URI: https://maxmanuel.com/
+Description: Plugin that shows git commit history
+Version: 1.0
+Author: Max Manuel
+Author URI: https://maxmanuel.com/
+Text domain: wp-home-test
+*/
