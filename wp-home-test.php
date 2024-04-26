7b6e14447ffbbcb7f450ed240f71306c89a60bd0
Max Manuel <maxcamina@gmail.com>
2024-04-26 01:05:59 -0500

    add an options page to display commits inside the wordpress menu

--- wp-home-test.php
+++ wp-home-test.php
@@ -8,3 +8,89 @@ Author: Max Manuel
 Author URI: https://maxmanuel.com/
 Text domain: wp-home-test
 */
+defined('ABSPATH') or die("Por aquí no vamos a ninguna parte");
+define('WPHT_DIR', plugin_dir_path(__FILE__));
+
+/**
+ * This class represents a WordPress settings page for display commits.
+ * It sets up the page and its contents, including sections and fields.
+ * The class uses WordPress hooks to add actions and filters to customize the settings page.
+ * The class includes methods to create the settings page, render its content,
+ * set up the sections and fields, and handle form submissions.
+ */
+class commits_Settings_Page
+{
+
+  public function __construct()
+  {
+    add_action('admin_menu', array($this, 'wph_create_settings'));
+    add_action('admin_init', array($this, 'wph_setup_sections'));
+    add_action('admin_init', array($this, 'wph_setup_fields'));
+  }
+
+  public function wph_create_settings()
+  {
+    $page_title = 'Commits';
+    $menu_title = 'Commits';
+    $capability = 'manage_options';
+    $slug = 'commits';
+    $callback = array($this, 'wph_settings_content');
+    $icon = 'dashicons-editor-ul';
+    $position = 2;
+    add_menu_page($page_title, $menu_title, $capability, $slug, $callback, $icon, $position);
+  }
+
+  public function wph_settings_content()
+  { ?>
+    <div class="wrap">
+      <h1>Commits</h1>
+      <?php settings_errors(); ?>
+      <form method="POST" action="options.php">
+        <?php
+        settings_fields('commits');
+        do_settings_sections('commits');
+        // submit_button();
+        ?>
+      </form>
+    </div> <?php
+          }
+
+          public function wph_setup_sections()
+          {
+            add_settings_section('commits_section', '', array(), 'commits');
+          }
+
+          public function wph_setup_fields()
+          {
+            $fields = array();
+            foreach ($fields as $field) {
+              add_settings_field($field['id'], $field['label'], array($this, 'wph_field_callback'), 'commits', $field['section'], $field);
+              register_setting('commits', $field['id']);
+            }
+          }
+
+          public function wph_field_callback($field)
+          {
+            $value = get_option($field['id']);
+            $placeholder = '';
+            if (isset($field['placeholder'])) {
+              $placeholder = $field['placeholder'];
+            }
+            switch ($field['type']) {
+              default:
+                printf(
+                  '<input name="%1$s" id="%1$s" type="%2$s" placeholder="%3$s" value="%4$s" />',
+                  $field['id'],
+                  $field['type'],
+                  $placeholder,
+                  $value
+                );
+            }
+            if (isset($field['desc'])) {
+              if ($desc = $field['desc']) {
+                printf('<p class="description">%s </p>', $desc);
+              }
+            }
+          }
+        }
+        new commits_Settings_Page();
